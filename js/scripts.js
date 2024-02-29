2c4fe8c66ae8f2bfc6b2b17d0487b55801d8aab3
Will-Maple17 <will.maple17@gmail.com>
2024-02-28 21:32:33 -0600

    Update scripts.js: added pokemonList Array

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Weedle', height: 0.3, types: ['bug', 'poison']},
+    {name: 'Pidgey', height: 0.3, types: ['flying', 'normal']}, 
+    {name: 'Bellsprout', height: 0.7, types: ['grass', 'poison']}
+ ]
