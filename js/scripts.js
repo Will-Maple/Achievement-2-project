2a1fc383b8006d2d5f51117b7263fd4cb22710b9
Will-Maple17 <will.maple17@gmail.com>
2024-03-26 16:44:02 -0500

    Update JSscript: added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ let pokemonList = [
     {name: 'Pidgey', height: 0.3, types: ['flying', 'normal']}, 
     {name: 'Bellsprout', height: 0.7, types: ['grass', 'poison']}
  ]
+
+ for (let i = 0; i < pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    if (pokemonList[i].height > 0.6) {
+        document.write(" - WOWZA! That's a Biggie!")
+    }
+    document.write("</p>")
+ }
