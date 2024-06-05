dbd114ce851a0fb8a341b9b4acbabcceea253cff
Will-Maple17 <will.maple17@gmail.com>
2024-06-05 03:54:31 -0500

    Update scripts.js forloop changed to ForEach

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,24 @@ let pokemonList = [
     {name: 'Bellsprout', height: 0.7, types: ['grass', 'poison']}
  ]
 
- for (let i = 0; i < pokemonList.length; i++) {
+function printArrayDetails(array) {
+ for (let i = 0; i < array.length; i++) {
     document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
     if (pokemonList[i].height > 0.6) {
         document.write(" - WOWZA! That's a Biggie!")
     }
     document.write("</p>")
  }
+}
+
+printArrayDetails(pokemonList);
+
+function dividing(dividend, divisor) {
+    if (divisor === 0) {
+        return "You're trying to divide by 0."
+    }
+    else {
+        let div = dividend / divisor;
+        return div;
+    }
+}
